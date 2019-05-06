a6a7c334e56b810675d98d3a19872ecc667d69a6
ASquirrelsTail <rjtwilton@hotmail.com>
2019-05-06 15:27:05 +0100

    Added Toast Flash messages for failed new user attempts

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
 from re import match
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 app.config['MONGO_DBNAME'] = 'cookbook'
 app.config['MONGO_URI'] = os.getenv('MONGO_URI')
+app.secret_key = os.getenv('SECRET_KEY')
 
 mongo = PyMongo(app)
 
@@ -20,8 +21,11 @@ def new_user():
     if request.method == 'POST':
         username = request.form.get('username', '')
         # Regex snipped for allowed characters from https://stackoverflow.com/questions/89909/how-do-i-verify-that-a-string-only-contains-letters-numbers-underscores-and-da
-        if (username != '' and match("^[A-Za-z0-9_-]*$", username) and
-                mongo.db.logins.find_one({'username': username}) is None):
+        if username == '' or not match("^[A-Za-z0-9_-]*$", username):
+            flash("Please enter a valid username!")
+        elif mongo.db.logins.find_one({'username': username}) is not None:
+            flash('Username "{}" is already taken, please choose another.'.format(username))
+        else:
             mongo.db.logins.insert_one(request.form.to_dict())
 
     return render_template('new-user.html')
