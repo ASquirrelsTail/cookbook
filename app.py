15a934cd2f76668e6acaf0e32816f023316bdd51
ASquirrelsTail <rjtwilton@hotmail.com>
2019-06-15 16:09:20 +0100

    Added custom error pages.

--- app.py
+++ app.py
@@ -754,6 +754,21 @@ def about():
     return render_template('about.html', username=session.get('username'))
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html', username=session.get('username')), 404
+
+
+@app.errorhandler(403)
+def page_forbidden(e):
+    return render_template('403.html', username=session.get('username')), 403
+
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('500.html', username=session.get('username')), 500
+
+
 if __name__ == '__main__':
     app.run(host = os.environ.get('IP'),
             port = os.environ.get('PORT'),
